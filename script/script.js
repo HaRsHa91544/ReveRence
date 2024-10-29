ac2870c592d787336814a1c13f86f92751791800
HaRsHa91544 <162473956+HaRsHa91544@users.noreply.github.com>
2024-10-30 00:56:42 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,19 +1,27 @@
-function change(){
-    var scrollValue=window.scrollY;
+function change() {
+    var scrollValue = window.scrollY;
     var nav = document.querySelector('header');
-    if(scrollValue>380){
-        
+    var slog = document.getElementById('slog');
+    var img = document.getElementById('image');
+    if (scrollValue > 380) {
+        nav.style.top = '0px';
         nav.classList.add('scrolled');
-    }
-    else{
+        img.src='img/logo.png';
+    } else if (scrollValue > 10) {
+        nav.style.top = '-100px'; // You can keep this if you want to hide the header while scrolling down
+        slog.style.opacity = '0'; // Ensure slog is visible when scrolling back up
         nav.classList.remove('scrolled');
+    } else {
+        nav.style.top = '40px';
+        slog.style.opacity = '1';
+        nav.classList.remove('scrolled');
+        img.src='img/logo2.png';
     }
-    console.log(scrollValue);
 }
-window.addEventListener('scroll',change);
+window.addEventListener('scroll', change);
 
 
-window.onload = function() {
+window.onload = function () {
     const rev = document.getElementById('rev');
     const lab = document.getElementById('lab');
     const pro = document.getElementById('pro');
@@ -22,20 +30,20 @@ window.onload = function() {
     let j = 0;
     let k = 0;
     let l = 0;
-    const interval = setInterval(function() {
+    const interval = setInterval(function () {
         if (i <= 1.2) {
             rev.innerHTML = i.toFixed(1);
             i += 0.1;
-        } 
-        else if(j<=500){
+        }
+        else if (j <= 500) {
             lab.innerHTML = `+${j}`;
             j += 20;
         }
-        else if(k<=31000){
+        else if (k <= 31000) {
             pro.innerHTML = `+${k}`;
             k += 1000;
         }
-        else if(l<=11000){
+        else if (l <= 11000) {
             del.innerHTML = `+${l}`;
             l += 500;
         }
