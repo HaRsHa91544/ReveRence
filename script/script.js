9e85f67eb86d40c492139a5883c3544379dad3b4
HaRsHa91544 <162473956+HaRsHa91544@users.noreply.github.com>
2024-11-10 18:55:45 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -21,10 +21,12 @@ window.onload = function () {
     const lab = document.getElementById('lab');
     const pro = document.getElementById('pro');
     const del = document.getElementById('del');
+    const phcc = document.getElementById('phcc');
     let i = 0;
     let j = 0;
     let k = 0;
     let l = 0;
+    let m=0;
     const interval = setInterval(function () {
         if (i <= 1.2) {
             rev.innerHTML = `₹${i.toFixed(1)}`;
@@ -41,6 +43,10 @@ window.onload = function () {
         else if (l <= 11000) {
             del.innerHTML = `+${l}`;
             l += 500;
+        }
+        else if (m <= 115) {
+            phcc.innerHTML = `+${m}`;
+            m += 5;
         }
         else {
             clearInterval(interval);
