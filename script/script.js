c55c437eeb864b738feb70c550b7e40551f490c3
HaRsHa91544 <162473956+HaRsHa91544@users.noreply.github.com>
2024-11-11 16:39:18 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,22 +1,13 @@
-/*function change() {
+var che = false;
+window.onscroll = function() {
     var scrollValue = window.scrollY;
-    var nav = document.querySelector('header');
-    var slog = document.getElementById('slog');
-    var img = document.getElementById('image');
-    if (scrollValue > 390) {
-        nav.style.top = '40px';
-        nav.classList.add('scrolled');
-    } else if (scrollValue > 10) {
-        nav.style.top = '40px'; 
-    } else {
-        nav.style.top = '40px';
-        nav.classList.remove('scrolled');
+    if(scrollValue>200 && !che){
+        che=true;
+        valchange();
     }
 }
-window.addEventListener('scroll', change);*/
 
-
-window.onload = function () {
+function valchange() {
     const rev = document.getElementById('rev');
     const lab = document.getElementById('lab');
     const pro = document.getElementById('pro');
@@ -26,7 +17,7 @@ window.onload = function () {
     let j = 0;
     let k = 0;
     let l = 0;
-    let m=0;
+    let m = 0;
     const interval = setInterval(function () {
         if (i <= 1.2) {
             rev.innerHTML = `₹${i.toFixed(1)}`;
