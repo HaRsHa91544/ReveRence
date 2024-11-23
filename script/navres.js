d234de50a0d15fe81e98c84b209c3fe7e42ca476
HaRsHa91544 <162473956+HaRsHa91544@users.noreply.github.com>
2024-11-23 13:27:21 +0530

    Add files via upload

--- script/navres.js
+++ script/navres.js
@@ -4,8 +4,23 @@ const drop1 = document.getElementById('dd1');
 const drop2 = document.getElementById('dd');
 const hdown = document.getElementById('hdown2');
 const sdown = document.getElementById('sdown2');
-var h=0,s=0;
-homebtn.onclick=function(){
+const menu = document.getElementById('menu-btn');
+var h=0,s=0,m=0;
+menu.onclick=function(){
+    if(m==0){
+        m=1;
+    }
+    else{
+        m=0;
+        homebtn.style.top='38%';
+        drop1.style.display='none';
+        hdown.style.transform='rotate(0deg)';
+        drop2.style.display='none';
+        solbtn.style.top='38%';
+        sdown.style.transform='rotate(0deg)';
+    }
+}
+homebtn.onclick=function hf(){
     if(h==0&&s==0){
         h=1;
         drop1.style.display='flex';
