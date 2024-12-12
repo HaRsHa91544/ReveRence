9c30ec107bbd7fa5dd76fdcc9689c3044dc1b9e3
HaRsHa91544 <162473956+HaRsHa91544@users.noreply.github.com>
2024-12-12 10:52:36 +0530

    Add files via upload

--- script/sol.js
+++ script/sol.js
@@ -18,14 +18,46 @@ function changeText(selectedRadio) {
         case 'sol4':
             hs2.textContent = 'R & D';
             hs4.textContent = 'Primary Health Care Centres';
-            break;  
-}
+            break;
+    }
 }
 
-// Get all radio buttons and add event listeners
 const radios = document.querySelectorAll('input[name="slider"]');
 radios.forEach(radio => {
-    radio.addEventListener('change', function() {
+    radio.addEventListener('change', function () {
         changeText(this);
     });
 });
+
+const solbox1 = document.getElementById('sol-box1');
+const solbox2 = document.getElementById('sol-box2');
+const solbox3 = document.getElementById('sol-box3');
+const solbox4 = document.getElementById('sol-box4');
+const content1 = document.getElementById('content-1');
+const content2 = document.getElementById('content-2');
+const content3 = document.getElementById('content-3');
+const content4 = document.getElementById('content-4');
+solbox1.onclick=function(){
+    content1.style.display = "block";
+    content2.style.display="none";
+    content3.style.display="none";
+    content4.style.display="none";
+}
+solbox2.onclick=function(){
+    content1.style.display = "none";
+    content2.style.display="block";
+    content3.style.display="none";
+    content4.style.display="none";
+}
+solbox3.onclick=function(){
+    content1.style.display = "none";
+    content2.style.display="none";
+    content3.style.display="block";
+    content4.style.display="none";
+}
+solbox4.onclick=function(){
+    content1.style.display = "none";
+    content2.style.display="none";
+    content3.style.display="none";
+    content4.style.display="block";
+}
